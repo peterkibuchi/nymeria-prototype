d862cd97b9c515858969060aa92cf1827e4f3bd8
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2025-07-23 14:11:29 +0300

    feat: optimize workflow and dx
    - add Renovate for automated dependency updates
    - configure CI pipeline to run automated format, lint and type checks
    - ignore ESLint and TS errors during builds as we already check for those separately in CI
    - add VSCode settings & recommendations

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
+import type { NextConfig } from "next";
+
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
  */
 import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const config: NextConfig = {
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
 
 export default config;
